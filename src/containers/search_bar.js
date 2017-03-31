2b3becd4b6f6eaec62d6c8e70bfcf2e7c7183f37
Syllinger <syllinger@gmail.com>
2017-03-31 02:04:13 -0400

    Connect fetch weather API action to search bar.

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends React.Component {
+class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     
     this.state = {term: ''};
-
-    //this.onInputChange = this.onInputChange.bind(this);
   }
 
   onInputChange(event) {
@@ -15,11 +16,14 @@ export default class SearchBar extends React.Component {
 
   onFormSubmit(event) {
     event.preventDefault();
+
+    this.props.fetchWeather(this.state.term);
+    this.setState({term: ''});
   }
 
   render() {
     return (
-      <form onSubmit={this.onFormSubmit} className="input-group">
+      <form onSubmit={this.onFormSubmit.bind(this)} className="input-group">
         <input 
           placeholder="Get a five-day foreccast in your favourite cities."
           className="form-control"
@@ -32,3 +36,9 @@ export default class SearchBar extends React.Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
